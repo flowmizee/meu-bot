4abbbae0b0ff38a9baad5f1918a0a0bff809f6c7
flowmizee <flowmize@gmail.com>
2025-08-17 17:24:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const { useSingleFileAuthState } = require('@whiskeysockets/baileys');
 const { handleMessage } = require('./flow');
 const qrcode = require('qrcode-terminal');
 
-const { state, saveState } = useSingleFileAuthState('./auth_info.json');
+const { state, saveCreds } = useSingleFileAuthState('./auth_info.json');
 
 async function startBot() {
   const sock = makeWASocket({
     auth: state,
-    printQRInTerminal: true // Apenas para testes locais
+    printQRInTerminal: true, // para ver QR no Termux
   });
 
   // Captura novas mensagens
@@ -25,9 +25,10 @@ async function startBot() {
 
   // Eventos de conexão
   sock.ev.on('connection.update', update => {
-    const { connection } = update;
+    const { connection, lastDisconnect } = update;
     if (connection === 'close') {
       console.log('Conexão fechada, tentando reconectar...');
+      // reconectar após 5 segundos
       setTimeout(() => startBot(), 5000);
     } else if (connection === 'open') {
       console.log('Bot conectado!');
@@ -35,7 +36,7 @@ async function startBot() {
   });
 
   // Salva credenciais automaticamente
-  sock.ev.on('creds.update', saveState);
+  sock.ev.on('creds.update', saveCreds);
 }
 
 // Inicia o bot
