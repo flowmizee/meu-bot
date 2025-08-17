85557bc2a986e7b74c63bbbd907665afe7c46588
flowmizee <flowmize@gmail.com>
2025-08-17 14:53:25 -0300

    Delete src/ocr.js

--- src/ocr.js
+++
@@ -1,8 +0,0 @@
-const Tesseract = require('tesseract.js');
-
-async function extractTextFromImage(imagePath) {
-  const result = await Tesseract.recognize(imagePath, 'por');
-  return result.data.text;
-}
-
-module.exports = { extractTextFromImage };
